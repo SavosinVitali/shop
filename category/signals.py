364d863f30f65858e06f51301e0197ab3020479d
savosinvitali <savosinvitali@gmail.com>
2020-10-15 18:54:41 +0300

    mac15102020

--- category/signals.py
+++ category/signals.py
@@ -10,6 +10,16 @@ from django.conf import settings
 from PIL import Image
 from sorl.thumbnail import get_thumbnail
 
+def file_name_generator(instance):
+    name = []
+    filebase, extension = instance.logo.name.split('.')
+    sizes = [(240, 240), (960, 960)]
+    name.append(settings.MEDIA_ROOT + '/' + instance.logo.name)
+    for size in sizes:
+        name.append(settings.MEDIA_ROOT + '/%s_%s.%s' % (filebase, size[0], extension))
+    return name
+
+
 @receiver(post_delete, sender=Brand)
 def logo_delete(sender, instance, **kwargs):
     if instance.logo.name:
@@ -27,26 +37,24 @@ def logo_add(sender, instance, **kwargs):
            sizes = [(240, 240), (960, 960)]
            old_self.logo.delete(False)
            for size in sizes:
-               name = settings.MEDIA_ROOT + '\%s_%s.%s' % (filebase, size[0], extension)
+               name = settings.MEDIA_ROOT + '/%s_%s.%s' % (filebase, size[0], extension)
                if os.path.isfile(name):
                    os.remove(name)
        if old_self.iso and instance.iso != old_self.iso:
           old_self.iso.delete(False)
-       if instance.name != old_self.name and os.path.isfile(settings.MEDIA_ROOT + '\\' + instance.logo.name):
+       if instance.name != old_self.name and os.path.isfile(settings.MEDIA_ROOT + '/' + instance.logo.name):
           new_path = upload_location_image(instance, instance.logo.name)
-          os.rename(settings.MEDIA_ROOT + '\\' + instance.logo.name, settings.MEDIA_ROOT + '\\' + new_path)
+          os.rename(settings.MEDIA_ROOT + '/' + instance.logo.name, settings.MEDIA_ROOT + '/' + new_path)
           instance.logo = new_path
 
 
 @receiver(post_save, sender=Brand)
 def resize_image(sender, instance, **kwargs):
-    print('-resize-')
     if instance.pk is not None and instance.logo:
-        filebase, extension = instance.logo.name.split('.')
-        sizes = [(240, 240), (960, 960)]
-        for size in sizes:
-            im = Image.open(instance.logo)
-            name = settings.MEDIA_ROOT + '\%s_%s.%s' % (filebase, size[0], extension)
-            im.thumbnail(size)
+        names = file_name_generator(instance)
+        print(names)
+        for name in names:
+            im = Image.open(names[0])
+            # im.thumbnail(size)
             im.save(name)
 
