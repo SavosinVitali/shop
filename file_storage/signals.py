dc9df00f19918b61f9a3e13b617cac0856340d56
savosinvitali <savosinvitali@gmail.com>
2020-12-29 14:29:24 +0300

    12

--- file_storage/signals.py
+++ file_storage/signals.py
@@ -1,8 +1,13 @@
+import sys
+
 from django.db.models.signals import post_delete, pre_save, post_save
 from django.dispatch.dispatcher import receiver
 from file_storage.models import File_Storage, upload_location_file, Image_Storage, upload_location_image
 import os
 from django.conf import settings
+from PIL import Image
+from io import StringIO, BytesIO
+from django.core.files.uploadedfile import InMemoryUploadedFile
 from pytils.translit import slugify
 
 # def file_name_generator(logo):
@@ -61,6 +66,17 @@ def image_rename(sender, instance, **kwargs):
                 os.renames(settings.MEDIA_ROOT + '/' + old_self.image.name, settings.MEDIA_ROOT + '/' + names)
                 instance.image = names
 
+        if instance.image.closed is False:
+            print('otkrita kartina')
+            image = instance.image
+            img = Image.open(image)
+            new_img = img.convert('RGB')
+            new_img.thumbnail((600,600), Image.ANTIALIAS)
+            output = BytesIO()
+            new_img.save(output, 'JPEG', quality=90)
+            output.seek(0)
+            instance.image = InMemoryUploadedFile(output,'ImageField', "%s.jpg" %instance.image.name, 'image/jpeg', sys.getsizeof(output), None)
+
         if old_self.image and old_self.image != instance.image:
             old_self.image.delete(False)
 
@@ -75,3 +91,11 @@ def file_delete(sender, instance, **kwargs):
     if instance.files:
        instance.files.close()
        instance.files.delete(False)
+
+
+# image = Img.open(StringIO.StringIO(self.photo.read()))
+#             image.thumbnail((200,200), Img.ANTIALIAS)
+#             output = StringIO.StringIO()
+#             image.save(output, format='JPEG', quality=75)
+#             output.seek(0)
+#             self.photo= InMemoryUploadedFile(output,'ImageField', "%s.jpg" %self.photo.name, 'image/jpeg', output.len, None)
