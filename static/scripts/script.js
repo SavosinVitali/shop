d32895f859af45741cd15a562bd43e62f76e8a35
savosinvitali <savosinvitali@gmail.com>
2021-02-10 17:12:41 +0300

    menu ok js start

---
+++ static/scripts/script.js
@@ -0,0 +1,47 @@
+// var obj1 = document.getElementById('sub_menu');
+//
+//
+// if (getComputedStyle(obj1).display === 'none') {
+// alert("wsddddd");
+// }
+var button = $('#sub_menu');
+if (button.is(":visible"))
+{
+     alert("none");  // Hide button
+}
+if (button.is(":visible"))
+{
+     alert("none");  // Hide button
+}
+
+//
+//
+// $(document).ready(function(){
+//
+//         var mode = document.getElementById('sub_menu');
+//
+//
+//  $("#sub_menu").each(function() {
+//     if ($(this).css("display") === "none") {
+//         // handle non visible state
+//         alert("none");
+//     } else {
+//         // handle visible state
+//     }
+// });
+
+
+    //
+    //     $("#sub_menu").click(function(){
+    //
+    //
+    //
+    //        alert("wsddddd");
+    //
+    //
+    //
+    //     });
+    //
+    //
+    //
+    // });
