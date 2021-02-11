86c236e79b58c73690031df96009b99cad66635b
savosinvitali <savosinvitali@gmail.com>
2021-02-11 17:21:49 +0300

    menu ok js ok

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -1,47 +1,44 @@
-// var obj1 = document.getElementById('sub_menu');
-//
-//
-// if (getComputedStyle(obj1).display === 'none') {
-// alert("wsddddd");
-// }
-var button = $('#sub_menu');
-if (button.is(":visible"))
-{
-     alert("none");  // Hide button
+$(document).ready(function(){
+
+
+wrapped();
+
+$(window).resize(function() {
+   wrapped();
+});
+
+function wrapped() {
+    var offset_top_prev;
+    $('.flex-item').each(function() {
+       var offset_top = $(this).offset().top;
+      if (offset_top > 12) {
+            if($(this).hasClass('wrapped')){
+
+             }else{
+
+                $(this).addClass('wrapped');
+                $(".drop-menu").css("display","block");
+                $(this).clone().appendTo(".drop-menu-touch:last");
+
+            }
+
+
+
+      } else if (offset_top == 12) {
+          if($(this).hasClass('wrapped'))
+          {
+              $(this).removeClass('wrapped');
+              $(".drop-menu-touch li:last").remove();
+          }
+          if ($(".drop-menu-touch").find('li').length == 0){
+              $(".drop-menu").css("display","none");
+          }
+          // $(".drop-menu").css("display","none");
+
+      }
+
+   offset_top_prev = offset_top;
+   });
 }
-if (button.is(":visible"))
-{
-     alert("none");  // Hide button
-}
 
-//
-//
-// $(document).ready(function(){
-//
-//         var mode = document.getElementById('sub_menu');
-//
-//
-//  $("#sub_menu").each(function() {
-//     if ($(this).css("display") === "none") {
-//         // handle non visible state
-//         alert("none");
-//     } else {
-//         // handle visible state
-//     }
-// });
-
-
-    //
-    //     $("#sub_menu").click(function(){
-    //
-    //
-    //
-    //        alert("wsddddd");
-    //
-    //
-    //
-    //     });
-    //
-    //
-    //
-    // });
+    });
