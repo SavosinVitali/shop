f900b81a321a6202052483af910b48ea94468449
savosinvitali <savosinvitali@gmail.com>
2021-03-12 17:52:18 +0300

    menu 4/6

--- static/scripts/script.js
+++ static/scripts/script.js
@@ -8,29 +8,29 @@
 }
 
 if (viewport().width >= 801) {
-    // console.log('1');
-    $('.menu-category').ready(function () {
-        sub_menu();
+    $(window).on("load", function () {
+
         wrapped_submenu();
-
-    });
-
-    $('.header-top-menu').ready(function () {
         wrapped();
+        sub_menu();
+
     });
 }
 
 $(document).ready(function(){
 
 $(window).resize(function() {
-   // width = $(window).width();
-    if (viewport().width >= 801) {
-        // $(".drop-menu").css("display","block");
-        // $(".drop-menu-category").css("display","flex");
-        wrapped();
-        sub_menu();
-        wrapped_submenu();
-      }
+
+
+        if (viewport().width >= 801) {
+
+            wrapped();
+            wrapped_submenu();
+            sub_menu();
+
+
+        }
+
     else{
         $('.menu-category').css("height",'auto');
         $(".menu-category-sub").children('li').css("flex-basis", 'auto');
@@ -39,16 +39,11 @@ $(window).resize(function() {
         $(".drop-menu").css("display","none");
         $(".drop-menu-category").css("display","none");
 
+
     }
-    // if (width <= 801) {
-    //     $('.flex-item').each(function() {
-    //         $(this).removeClass('wrapped');
-    //     });
-    //
-    //   }
+
 
 });
-
 
     });
 
@@ -95,10 +90,14 @@ function wrapped_submenu() {
 
     var A = $('.menu-category > li');
     var offset_top_prev= A.first('li').offset().top;
-   A.each(function() {
+    var i=0;
+
+
+   A.each(function(index) {
+
        var offset_top = $(this).offset().top;
        if (offset_top > offset_top_prev) {
-             $('.menu-category').css("height",A.first().height());
+             // $('.menu-category').css("height",A.first().height());
 
             if($(this).hasClass('wrapped_submenu')){
                 $(".drop-menu-category").css("display","flex");
@@ -108,7 +107,10 @@ function wrapped_submenu() {
                 $(this).clone().appendTo(".drop-menu-touch-category:last");
                }
 
-      } else if (offset_top == offset_top_prev) {
+      } else {
+            i=i+1;
+
+          if (offset_top == offset_top_prev) {
           if ($(this).hasClass('wrapped_submenu')) {
               $(this).removeClass('wrapped_submenu');
               $(".drop-menu-touch-category > li").last().remove();
@@ -118,18 +120,34 @@ function wrapped_submenu() {
               }
 
           }
-      }
+      }}
    });
+
+
+    $('.menu-category').css("height",A.first().height());
     $(".drop-menu-touch-category ").css("top",A.first('li').height());
     $(".header-category").css("visibility","visible");
+    console.log( parseInt($(".header-category li").first().css( "flex-basis" )));
+     if ( ($(".header-category").width()/i) > parseInt($(".header-category li").first().css( "flex-basis" ))){
+        $('.menu-category > li').css("width",($(".header-category").width()/i ) );
+        // $(".drop-menu-category").css("display","none");
+        // $('.menu-category > li').css("width",($(".header-category").width()/6) );
+
+        // console.log(i);
+    }
+
+
 
 }
 
 function sub_menu() {
     var A = $('.menu-category > li');
     $(".menu-category-sub").children('li').css("flex-basis", 0);
-    $(".menu-category-sub").css("left",$(".menu-category").children('li').first().offset().left);
-    $(".menu-category-sub").css("width",$(".header-category").width()-($('.menu-category-sub').outerWidth(true) - $('.menu-category-sub').width()));
+    // $(".menu-category-sub").css("left",$(".menu-category").offset().left);
+
+    // $(".menu-category-sub").css("left",'0');
+    // $(".menu-category-sub").css("width",$(".header-category").width()-($('.menu-category-sub').outerWidth(true) - $('.menu-category-sub').width()));
+
        A.each(function () {
        $(this).children('ul').children(".hidden").remove();
         var max = 0, max_real = 0, i = 0, t = 0, r = 0, z = 0, pad = 0;
@@ -158,4 +176,13 @@ function sub_menu() {
         }
     });
 
+
+
+$(".menu-category-sub").css("left",$(".menu-category").offset().left);
+ if ($('.drop-menu-category').is(":visible")){
+        $(".menu-category-sub").css("width", $(".menu-category").width()+$('.drop-menu-category').width());
+    }else {
+        $(".menu-category-sub").css("width", $(".menu-category").width());
+
+    }
 }
